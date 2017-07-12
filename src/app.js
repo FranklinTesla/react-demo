c98e57195f52cf09d6202a959f89e7ab1adf5194
张祥闰 <hzzhangxiangrun@corp.netease.com>
2017-07-12 19:00:16 +0800

    [learn redux]: http://redux.js.org/docs/basics/Store.html

--- src/app.js
+++ src/app.js
@@ -1,102 +1,72 @@
-/**
- * ------------------------------------
- * @param {}
- * @export 导出变量
- * ------------------------------------
- */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
 
-function shouldUpdate(WrappedComponent) {
-    return class extends React.Component {
-        shouldComponentUpdate(nextProps) {
-            const props = this.props;
-            for (let key in props) {
-                if (!props.hasOwnProperty(key)) {
-                    continue;
-                }
-                if (nextProps[key] !== props[key]) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        render() {
-            return <WrappedComponent {...this.props}/>
-        }
+const list1 = [
+    {name: 'bj'},
+    {name: 'zo'},
+    {name: 'qxf'},
+    {name: 'zxr'},
+    {name: 'djr'},
+];
+const list2 = [
+    {name: 'rl'},
+    {name: 'fy'},
+    {name: 'lkx'},
+    {name: 'zfy'}
+];
+
+function reducer1(state = list1, action) {
+    if (action.type === 'change') {
+        return state === list1? list2: list1;
     }
+    return state;
 }
 
-const A = shouldUpdate(class extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log('A is created')
+function reducer2(state = list2, action) {
+    if (action.type === 'change') {
+        return state === list1? list2: list1;
     }
-    componentWillUpdate() {
-        console.log('A will be updated')
-    }
-    render() {
-        return (
-            <p>{this.props.text}</p>
-        )
-    }
-});
+    return state;
+}
 
-const B = shouldUpdate(class extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log('B is created')
-    }
-    componentWillUpdate() {
-        console.log('B will be updated')
-    }
-    render() {
-        return (
-            <p>{this.props.text}</p>
-        )
-    }
-});
+const store = createStore(combineReducers({
+    list1: reducer1,
+    list2: reducer2
+}));
 
-
-
-class Container extends React.Component {
-    constructor(props) {
-        super(props);
+class List1 extends React.Component {
+    constructor() {
+        super();
+        let state = store.getState();
         this.state = {
-            textA: "I'm a",
-            textB: "I'm b"
+            list: state.list2
         }
+        store.subscribe(() => {
+            this.setState({
+                list: store.getState().list2
+            });
+        });
+        this.changeList.bind(this);
+    }
+    changeList() {
+        store.dispatch({
+            type: 'change'
+        });
     }
     render() {
+        let state = this.state;
         return (
             <div>
-                <button onClick={() => this.changeA()}>changeA</button>
-                <button onClick={() => this.changeB()}>changeB</button>
-                <A text={this.state.textA}/>
-                <B text={this.state.textB}/>
+                {
+                    state.list.map((item, index) => <p key={index} onClick={this.changeList}>{item.name}</p>)
+                }
             </div>
         )
     }
-    changeA() {
-        this.setState(function(prevState) {
-            return {
-                textA: prevState.textA === 'change a'?
-                            "I'm a": 'change a'
-            }
-        });
-    }
-    changeB() {
-        this.setState(function(prevState) {
-            return {
-                textB: prevState.textB === 'change b'?
-                    "I'm b": 'change b'
-            }
-        });
-    }
 }
 
-
 ReactDOM.render(
-    <Container/>,
+    <List1/>,
     document.getElementById('root')
 )
